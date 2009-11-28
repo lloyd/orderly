324d7c2b5ed3f300eb5c7e14142baef0f13284a2
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-28 15:34:44 -0700

    add running of parse_test binary across all test cases

--- test/run_tests.rb
+++ test/run_tests.rb
@@ -6,6 +6,10 @@ lexBinary = File.join(thisDir, "..", "build", "test", "bins", "lex", "lex_test")
 if !File.executable? lexBinary
   throw "Can't find lex test binary: #{lexBinary}"
 end
+parseBinary = File.join(thisDir, "..", "build", "test", "bins", "parse", "parse_test")
+if !File.executable? parseBinary
+  throw "Can't find parse test binary: #{parseBinary}"
+end
 
 passed = 0
 total = 0
@@ -13,29 +17,34 @@ total = 0
 puts "Running parse/lex tests:"
 
 Dir.glob(File.join(casesDir, "*.orderly")).each { |f| 
-  got = ""
-  IO.popen(lexBinary, "w+") { |lb|
-    File.open(f, "r").each {|l| lb.write(l)}
-    lb.close_write
-    got = lb.read
-  }
-  wantFile = f.sub(/orderly$/, "lexed")
+  [
+    [ "Lexing", f.sub(/orderly$/, "lexed"), lexBinary ],
+    [ "Parsing", f.sub(/orderly$/, "parsed"), parseBinary ]
+  ].each { |testType|
+    what, wantFile, program = *testType
+    got = ""
+    IO.popen(program, "w+") { |lb|
+      File.open(f, "r").each {|l| lb.write(l)}
+      lb.close_write
+      got = lb.read
+    }
 
-  print "<#{f.sub(/^.*?([^\/]+)\.orderly$/, '\1')}>:\t "
-  if !File.exist? wantFile
-    puts "FAIL"    
-    puts "'goldfile' doesn't exist: #{wantFile}"
-    puts got
-  else
-    want = IO.read(wantFile)
-    if (got == want)
-      puts "ok"
-      passed += 1
+    print "#{what} <#{f.sub(/^.*?([^\/]+)\.orderly$/, '\1')}>:\t "
+    if !File.exist? wantFile
+      puts "FAIL"    
+      puts "'goldfile' doesn't exist: #{wantFile}"
+      puts got
     else
-      puts "FAIL"
-      # XXX: give more information
+      want = IO.read(wantFile)
+      if (got == want)
+        puts "ok"
+        passed += 1
+      else
+        puts "FAIL"
+        # XXX: give more information
+      end
     end
-  end
+  }
   total += 1
 }
 
