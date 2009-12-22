b200f2890b2bee9c9021f9cc891cd8527473875a
Greg Olszewski <gno@gropeep.org>
2009-12-21 19:12:37 -0800

    new test harness, accept binary dir also

--- test/run_positive_tests.rb
+++ test/run_positive_tests.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
+binaryDir = ENV["BINARY_DIR"]
+
 # arguments are a string that must match the test name
 substrpat = ARGV.length ? ARGV[0] : ""
 
 thisDir = File.dirname(__FILE__)
 casesDir = File.join(thisDir, "positive_cases")
-reformatBinary = File.join(thisDir, "..", "build", "reformatter", "orderly_reformat")
+reformatBinary = File.join(binaryDir, "reformatter", "orderly_reformat")
 if !File.executable? reformatBinary
   throw "Can't find orderly_reformat binary: #{reformatBinary}"
 end
