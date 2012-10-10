8015504011aa443ac987221f156a758e7e0a1398
Greg Olszewski <gno@sugar>
2012-10-10 00:43:03 -0700

    Documentation is worth it's weight in gold.

--- src/api/ajv_parse.h
+++ src/api/ajv_parse.h
@@ -55,7 +55,9 @@ ORDERLY_API yajl_status ajv_parse_complete(ajv_handle hand);
 ORDERLY_API void ajv_free_error(ajv_handle hand, unsigned char *err);
 ORDERLY_API unsigned int ajv_get_bytes_consumed(ajv_handle hand);
 typedef int (*ajv_format_checker)(const char *string, unsigned int length);
-
+  /* Register a callback to give meaning to value for the "format" 
+     key. */
+    
 ORDERLY_API void ajv_register_format(const char *name, ajv_format_checker checker);
   
 #ifdef __cplusplus
